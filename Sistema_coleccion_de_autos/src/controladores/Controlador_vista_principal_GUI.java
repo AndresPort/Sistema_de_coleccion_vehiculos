e1a916fc40101e5f04197c4fefc74a550a93a5a2
AndresPort <andresfelipe29portillo@gmail.com>
2024-04-17 17:03:47 -0500

    Se termino completamente la clase Controlador_vista_principal_GUI

--- Sistema_coleccion_de_autos/src/controladores/Controlador_vista_principal_GUI.java
+++ Sistema_coleccion_de_autos/src/controladores/Controlador_vista_principal_GUI.java
@@ -3,11 +3,12 @@ import modelos.Coleccion_carros;
 import java.awt.event.ActionEvent;
 import vistas.Vista_principal_GUI;
 import java.awt.event.ActionListener;
+import javax.swing.JOptionPane;
 
 public class Controlador_vista_principal_GUI implements ActionListener {
     
     private Vista_principal_GUI vista_principal;
-    private Coleccion_carros coleccion_carros
+    private Coleccion_carros coleccion_carros;
     
     //Metodos
     //metodo constructor
@@ -17,7 +18,7 @@ public class Controlador_vista_principal_GUI implements ActionListener {
         //Hacer visible la vista_principal
         this.vista_principal.setVisible(true);
         //instanciar el objeto coleccion
-        this.coleccion_carros= new Coleccion_Carros();
+        this.coleccion_carros= new Coleccion_carros();
         
         //Escuchas que interpretan las acciones sobre los botones
         this.vista_principal.jbtn_agregar.addActionListener(this);
@@ -40,17 +41,35 @@ public class Controlador_vista_principal_GUI implements ActionListener {
             int ano_serie= Integer.parseInt(this.vista_principal.jtf_ano_serie.getText());
             int num_serie= Integer.parseInt(this.vista_principal.jtf_num_serie.getText());
             
-            //
+            //Llamado a la clase colecion_autos al metodo borrar_auto
+            int resultado=this.coleccion_carros.borrar_auto_de_la_lista(ano_serie, num_serie);
+            
+            //Mensaje que se va a mostrar segun si se pudo completar correctamente la operacion
+            if(resultado==1){
+                JOptionPane.showMessageDialog(null, "Resultado", "Se ha borrado el carro correctamente", 1);
+            }
+            else{
+                JOptionPane.showMessageDialog(null, "Resultado", "No se pudo encontrar el carro", 1);
+            }
+            
         }
         
         //Lo que va a pasar si se oprime el boton buscar auto
         if(e.getSource()==this.vista_principal.jbtn_buscar){
+            //Obtener los datos de los de los TextFields de la vista_principal
+            int ano_serie= Integer.parseInt(this.vista_principal.jtf_ano_serie.getText());
+            int num_serie= Integer.parseInt(this.vista_principal.jtf_num_serie.getText());
+            //Llamado a la clase colecion_autos al metodo borrar_auto
+            String resultado=this.coleccion_carros.buscar_auto_en_la_lista(ano_serie, num_serie);
             
+            JOptionPane.showMessageDialog(null, "Resultado", resultado, 1);
         }
         
         //Lo que va a pasar si se oprime el boton mostrar coleccion
         if(e.getSource()==this.vista_principal.jbtn_mostrar_coleccion){
-            
+            String resultado=this.coleccion_carros.mostrar_coleccion();
+    
+            JOptionPane.showMessageDialog(null, "Resultado", resultado, 1);
         }
         
     }
