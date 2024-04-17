e36d2dc4c5385289b0fb577de84c83fc35a4219c
AndresPort <andresfelipe29portillo@gmail.com>
2024-04-17 15:20:40 -0500

    Se creo el controlador de la vista principal y la vista principal, se conectaron las clases y se comprueba la funcionalidad

---
+++ Sistema_coleccion_de_autos/src/controladores/Controlador_vista_principal_GUI.java
@@ -0,0 +1,56 @@
+package controladores;
+import java.awt.event.ActionEvent;
+import vistas.Vista_principal_GUI;
+import java.awt.event.ActionListener;
+
+public class Controlador_vista_principal_GUI implements ActionListener {
+    
+    private Vista_principal_GUI vista_principal;
+    //Agregar el objeto coleccion
+    
+    public Controlador_vista_principal_GUI(){
+        //instancia del objeto vista_principal
+        this.vista_principal = new Vista_principal_GUI();
+        //Hacer visible la vista_principal
+        this.vista_principal.setVisible(true);
+        //instanciar el objeto coleccion
+        
+        
+        //Escuchas que interpretan las acciones sobre los botones
+        this.vista_principal.jbtn_agregar.addActionListener(this);
+        this.vista_principal.jbtn_borrar.addActionListener(this);
+        this.vista_principal.jbtn_buscar.addActionListener(this);
+        this.vista_principal.jbtn_mostrar_coleccion.addActionListener(this);
+        
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        //Lo que va a pasar si se oprime el boton agregar auto
+        if (e.getSource()== this.vista_principal.jbtn_agregar){
+            
+        }
+        
+        //Lo que va a pasar si se oprime el boton borrar auto
+        if (e.getSource()== this.vista_principal.jbtn_borrar){
+            //Obtener los datos de los de los TextFields de la vista_principal
+            int ano_serie= Integer.parseInt(this.vista_principal.jtf_ano_serie.getText());
+            int num_serie= Integer.parseInt(this.vista_principal.jtf_num_serie.getText());
+            
+            //
+        }
+        
+        //Lo que va a pasar si se oprime el boton buscar auto
+        if(e.getSource()==this.vista_principal.jbtn_buscar){
+            
+        }
+        
+        //Lo que va a pasar si se oprime el boton mostrar coleccion
+        if(e.getSource()==this.vista_principal.jbtn_mostrar_coleccion){
+            
+        }
+        
+    }
+    
+    
+}
--- Sistema_coleccion_de_autos/src/main/Main.java
+++ Sistema_coleccion_de_autos/src/main/Main.java
@@ -1,7 +1,9 @@
 package main;
 
+import controladores.Controlador_vista_principal_GUI;
 public class Main {
     public static void main(String args[]){
+        Controlador_vista_principal_GUI controlador_vista_principal= new Controlador_vista_principal_GUI();
         
     }
 }
